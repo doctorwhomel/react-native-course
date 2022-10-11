ad61a2d26dddeb5a69753fd1338453cdb95e07ac
WHOMEL <williamhomel@gmail.com>
2022-10-11 13:18:18 -0700

    animatable

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,20 +1,23 @@
 import { ScrollView, Text } from "react-native";
 import { Card } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 const ContactScreen = () => {
   return (
     <ScrollView>
-      <Card wrappStyle={{ margin: 10 }}>
-        <Card.Title>
-          <Text>Contact Information</Text>
-        </Card.Title>
-        <Card.Divider />
-        <Text>1 Nucamp Way</Text>
-        <Text>Seattle, WA</Text>
-        <Text style={{ marginBottom: 10 }}>U.S.A</Text>
-        <Text>Phone: 1-206-555-1234</Text>
-        <Text>Email: campsites@nucamp.co</Text>
-      </Card>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card wrappStyle={{ margin: 10 }}>
+          <Card.Title>
+            <Text>Contact Information</Text>
+          </Card.Title>
+          <Card.Divider />
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+        </Card>
+      </Animatable.View>
     </ScrollView>
   );
 };
